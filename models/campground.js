fbe3502bd5fbe36f888e8f83cbdb1f0c9ac65633
AtharvaPatil <atharvapatil0052@gmail.com>
2022-01-31 16:43:24 +0530

    Till Configuring sessions #489

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -14,6 +15,17 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+// This is to delete reviews with the ids in our deleted campground (doc)
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
 // Campground is model and CampgroundSchema is schema
---
+++ routes/campgrounds.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const {campgroundSchema} = require('../schemas.js');
+
+const validateCampground = (req, res, next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+
+
+router.get('/', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+}));
+
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+//This should be before the later one because it will hit the id route and wont find anything and we don't want that
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`)
+}));
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    res.render('campgrounds/show', {campground});
+}));
+
+// router.get('/makecampground', catchAsync(async (req, res) => {
+//     const camp = new Campground({
+//         title: 'My yard',
+//         description: 'nice camping'
+//     })
+//     await camp.save();
+//     res.send(camp);
+// }));
+
+router.get('/:id/edit', catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+}));
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+}));
+
+router.delete('/:id', catchAsync(async(req, res) => {
+    const{id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+}));
+
+module.exports = router;
