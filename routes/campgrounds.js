bc01ebc9b827a294dbc6cc0622b6c5fdfc342ccc
AtharvaPatil <atharvapatil0052@gmail.com>
2022-02-05 14:55:12 +0530

    Till the multer middleware #529

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,18 +7,18 @@ const Campground = require('../models/campground');
 const {campgroundSchema} = require('../schemas.js');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 
-
-
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 //isAuthenticated() is coming from passport module
 
 //This should be before the later one because it will hit the id route and wont find anything and we don't want that
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 // router.get('/makecampground', catchAsync(async (req, res) => {
 //     const camp = new Campground({
 //         title: 'My yard',
@@ -30,8 +30,6 @@ router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
