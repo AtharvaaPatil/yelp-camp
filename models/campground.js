6fa243ccbe533a3cce3e18b1f7efc86d733dfb02
AtharvaPatil <atharvapatil0052@gmail.com>
2022-02-07 00:02:06 +0530

    Till Registering for mapbox #542

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,20 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema ({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+})
+//this is so that we don't load the whole image we just show the width 200 version which is light weight and won't take long to load
+
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
