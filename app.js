0d6064b2192a43a7ebcbe5c7fab044e70e833c28
AtharvaPatil <atharvapatil0052@gmail.com>
2022-02-08 21:42:28 +0530

    Till Hiding errors #567

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const passport = require('passport');
 const LocalStratergy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -43,13 +45,18 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeasecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
